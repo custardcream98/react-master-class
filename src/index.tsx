829afdee1409d389869b9d5a0f01103db463a539
custardcream@kakao.com <custardcream@kakao.com>
2022-07-24 05:09:44 +0900

    Theme in TS

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { lightTheme, darkTheme } from "./theme";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider > */}
+    <ThemeProvider theme={darkTheme}>
       <App />
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
